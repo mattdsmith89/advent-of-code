bc45f66551aa4d24d2180ea0e3ada0fe28b36439
Matt Smith <matt.d.smith.89@gmail.com>
2020-12-01 13:04:19 +0000

    Tuples

--- 2020/day-01/Program.fs
+++ 2020/day-01/Program.fs
@@ -13,18 +13,18 @@ let splitLine = fun (line: string) ->
     |> List.ofArray
 
 let folder head acc elem =
-    if head + elem = 2020 
-    then head * elem 
+    if head + elem = 2020
+    then (head, elem, head * elem)
     else acc
 
 let rec sumCheckMultiply list =
     match list with
     | head::tail ->
-        let result = Seq.fold (folder head) 0 tail
+        let result = Seq.fold (folder head) (0, 0, 0) tail
         match result with
-        | 0 -> sumCheckMultiply tail
+        | (0, 0, 0) -> sumCheckMultiply tail
         | _ -> result
-    | [] -> 0
+    | [] -> (0, 0, 0)
 
 let testInput = "1721
 979
@@ -48,6 +48,8 @@ let realResult = sumCheckMultiply realInput
 
 [<EntryPoint>]
 let main argv =
-    printfn "Test Result is %i" testResult
-    printfn "Real Result is %i" realResult
+    let (testA, testB, testValue) = testResult
+    let (realA, realB, realValue) = realResult
+    printfn "Part 1 Test Result is %i from %i and %i" testValue testA testB
+    printfn "Part 1 Real Result is %i from %i and %i" realValue realA realB
     0 // return an integer exit code
