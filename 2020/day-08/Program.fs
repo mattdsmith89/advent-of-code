b627ec2838f2b994a7af905f886247673e01e41e
Matt Smith <matt.d.smith.89@gmail.com>
2020-12-09 08:38:28 +0000

    Tidy

--- 2020/day-08/Program.fs
+++ 2020/day-08/Program.fs
@@ -16,45 +16,45 @@ module Solver =
         | BadInstruction
         | UnknownOp
 
-    let rec readInstructionLoop (visited: int Set) acc next (instructions: string list) =
-        if visited.Contains next then (acc, Infinite)
-        else if next = instructions.Length then (acc, Normal)
-        else if next < 0 || next > instructions.Length then (acc, BadInstruction)
-        else
-        let newVisited = visited.Add next
-        let instruction = instructions.[next]
-        let operation = instruction.[..2]
-        let argument = int instruction.[4..]
-        match operation with
-        | "nop" -> readInstructionLoop newVisited acc (next + 1) instructions
-        | "acc" -> readInstructionLoop newVisited (acc + argument) (next + 1) instructions
-        | "jmp" -> readInstructionLoop newVisited acc (next + argument) instructions
-        | _ -> (acc, UnknownOp)
-
     let readInstructions =
+        let rec readInstructionLoop (visited: int Set) acc next (instructions: string list) =
+            if visited.Contains next then (acc, Infinite)
+            else if next = instructions.Length then (acc, Normal)
+            else if next < 0 || next > instructions.Length then (acc, BadInstruction)
+            else
+            let newVisited = visited.Add next
+            let instruction = instructions.[next]
+            let operation = instruction.[..2]
+            let argument = int instruction.[4..]
+            match operation with
+            | "nop" -> readInstructionLoop newVisited acc (next + 1) instructions
+            | "acc" -> readInstructionLoop newVisited (acc + argument) (next + 1) instructions
+            | "jmp" -> readInstructionLoop newVisited acc (next + argument) instructions
+            | _ -> (acc, UnknownOp)
+
         readInstructionLoop Set.empty 0 0
 
-    let swapOperation (instruction: string) =
-        let operation = instruction.[..2]
-        let argument = instruction.[4..]
-        match operation with
-        | "nop" -> "jmp " + argument
-        | "jmp" -> "nop " + argument
-        | _ -> instruction
-
-    let swapInstruction next =
-        List.mapi (fun i x -> if i = next then swapOperation x else x)
-
-    let rec runTweaksLoop next (instructions: string list) =
-        let tweakedInstructions = swapInstruction next instructions
-        let result = readInstructions tweakedInstructions
-        match result |> snd with
-        | Infinite -> runTweaksLoop (next + 1) instructions
-        | BadInstruction -> runTweaksLoop (next + 1) instructions
-        | Normal -> result |> fst
-        | UnknownOp -> failwith "instructions contained unknown operation" 
-
     let runTweaks =
+        let swapOperation (instruction: string) =
+            let operation = instruction.[..2]
+            let argument = instruction.[4..]
+            match operation with
+            | "nop" -> "jmp " + argument
+            | "jmp" -> "nop " + argument
+            | _ -> instruction
+
+        let swapInstruction next =
+            List.mapi (fun i x -> if i = next then swapOperation x else x)
+
+        let rec runTweaksLoop next (instructions: string list) =
+            let tweakedInstructions = swapInstruction next instructions
+            let result = readInstructions tweakedInstructions
+            match result |> snd with
+            | Infinite -> runTweaksLoop (next + 1) instructions
+            | BadInstruction -> runTweaksLoop (next + 1) instructions
+            | Normal -> result |> fst
+            | UnknownOp -> failwith "instructions contained unknown operation" 
+
         runTweaksLoop 0
 
 [<EntryPoint>]
