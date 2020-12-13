9f66a32d587540ab1ca0b421eef679daa787c090
Matt Smith <matt.d.smith.89@gmail.com>
2020-12-13 15:15:17 +0000

    Day 13 part 2

--- 2020/day-13/Program.fs
+++ 2020/day-13/Program.fs
@@ -23,6 +23,33 @@ let findBus timestamp buses =
 let parseBuses (input: string) =
     input.Split(',') |> Array.filter (fun x -> x <> "x") |> Seq.map int |> List.ofSeq
 
+let parseBuses2 (input: string) =
+    input.Split(',') 
+    |> Array.mapi (fun i x -> (x, int64 i)) 
+    |> Array.choose (fun (x, i) -> if x = "x" then None else Some (int64 x, (int64 x) - i))
+    |> List.ofArray
+
+let extgcd a b =
+    let rec inner (r'', s'', t'') (r', s', t') =
+        let step () =
+            let q = r'' / r'
+            let r = r'' - q*r'
+            let s = s'' - q*s'
+            let t = t'' - q*t'
+            (r, s, t)
+        if r' = 0L then (r'', s'', t'')
+        else inner (r', s', t') (step())
+    inner (a, 1L, 0L) (b, 0L, 1L)
+
+let chinese (pairs: (int64 * int64) list) =
+    let n = pairs |> List.map fst |> List.reduce ( * )
+    let ays = pairs |> List.map snd
+    let ns = pairs |> List.map fst
+    let ys = ns |> List.map (fun x -> n / x)
+    let zs = List.map2 extgcd ns ys |> List.map (fun (_, _, b) -> b)
+    let x = List.map3 (fun a y z -> a * y * z) ays ys zs |> List.sum 
+    ((x % n) + n) % n
+
 [<EntryPoint>]
 let main argv =
     let input = Helpers.readLines "input.txt" |> List.ofSeq
@@ -30,4 +57,6 @@ let main argv =
     let buses = parseBuses input.[1]
     let (busTime, busId) = findBus startTimestamp buses
     printfn "result1 %i" ((busTime - startTimestamp) * busId)
+    let result2 = chinese (parseBuses2 input.[1])
+    printfn "result2 %i" result2
     0 // return an integer exit code
